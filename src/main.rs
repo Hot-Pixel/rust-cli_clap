6da7e79d739fed3d15a4b338798eddf2cb09482a
Santiago Martinez <santiagomartinezlt@gmail.com>
2025-05-19 17:10:00 +0200

    Sync

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use clap::Parser;
+use colored::Colorize;
+
+#[derive(Parser)]
+struct Options {
+    #[clap(default_value = "Meow", short = 'm', long = "message")]
+    /// What does the cat say?
+    message: String,
+    #[clap(short = 'd', long = "dead")]
+    /// Make the cat appear dead
+    dead: bool,
+}
+
+fn main() {
+    let options = Options::parse();
+
+    let message = options.message;
+    let eye =  if options.dead { "x" } else { "o" };
+    
+    if message.to_lowercase().contains("woof") {
+        eprintln!("Cats do not woof");
+    }
+
+    println!("{}", message);
+    println!(" \\");
+    println!("  \\");
+    println!("     /\\_/\\");
+    println!("    ( {eye} {eye} )");
+    println!("    =( I )=");
+}
